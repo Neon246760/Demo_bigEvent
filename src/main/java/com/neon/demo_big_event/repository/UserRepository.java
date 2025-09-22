f4f6ff41f38cebbe2724b5b7d5180ea12a6e6354
0xneon <0xneon@example.com>
2025-09-22 22:58:23 +0800

    Change the return value type of User

--- src/main/java/com/neon/demo_big_event/repository/UserRepository.java
+++ src/main/java/com/neon/demo_big_event/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.neon.demo_big_event.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +7,6 @@ import com.neon.demo_big_event.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    User findByUsername(String username);
     boolean existsByUsername(String username);
 }
